9339d0bbed86190a4d5bbc228eaf50eb0d7dda4a
BuiRai <BuiRai@BuiRai-PC.lan>
2014-11-20 21:27:30 -0600

    Cache con librerias Se eliminan bien a los candidatos

---
+++ src/Modelo/AdminVotos.java
@@ -0,0 +1,117 @@
+package Modelo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import ArquiSoftware.CacheCandidatos;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author a09001005
+ */
+public class AdminVotos extends Modelo {
+
+    static AdminVotos adminVtos;
+    private ArrayList<Candidato> cands;
+    private int contador = 0;
+    private CacheCandidatos cache;
+
+    
+    public AdminVotos() {
+        this.cands = new ArrayList();
+        cache = new CacheCandidatos("CandidateInformation");
+        inicializarCandidatos();
+        inicializarEventos();
+        super.datos = cands;
+    }
+
+    public static AdminVotos getInstance() {
+        if (adminVtos == null) {
+            adminVtos = new AdminVotos();
+        }
+        return adminVtos;
+    }
+
+    public void inicializarCandidatos() {
+        Candidato A = new Candidato("A");
+        cands.add(A);
+        Candidato B = new Candidato("B");
+        cands.add(B);
+        Candidato C = new Candidato("C");
+        cands.add(C);
+    }
+
+    private void inicializarEventos() {
+        int numeroDeEventos=3;
+        for (int i = 0; i < numeroDeEventos; i++) {
+            eventos.add(new Evento(i));
+        }
+    }
+
+
+    public void agregarCandidatos(ArrayList<Candidato> candidatos) {
+        super.setDatos(candidatos);
+        notificarObservadoresEvento(0);
+    }
+
+    //Retorna un candidato de la cache
+    public Object getCandidatoEnCache(String nombre){
+        return cache.getCandidato(nombre);
+    }
+    
+    //Agrega un candidato a la cache
+    public void agregarCandidatoEnCache(Candidato candidato){
+        cache.agregarCandidato(candidato);
+    }
+    
+    //Verifica si el candidato existe o no en la cache
+    public boolean existenciaDeCandidato(String nombre){
+        return cache.existenciaDeCandidato(nombre);
+    }
+
+    /**
+     * Aqui se deberia de buscar al candidato en el cache 
+     * @param nombre nombre del candidato
+     */
+    public void agregarVoto(String nombre) {
+        if (existenciaDeCandidato(nombre)) {
+            Candidato cndt = (Candidato)getCandidatoEnCache(nombre);
+            cndt.agregarVoto();
+            System.out.println("LOCALIZACION: Se localizo en la cache, pues ya se habia votado antes por el");
+        }else{
+            for (Candidato cand : (ArrayList<Candidato>) getDatos()) {
+                if (cand.getNombre().equals(nombre)) {
+                    cand.agregarVoto();
+                    System.out.println("LOCALIZACION: Se localizo en el ArrayList, pues es primera vez que se vota por el");
+                    agregarCandidatoEnCache(cand);
+                }
+            }
+        }
+        
+        notificarObservadoresEvento(0);
+    }
+
+    public void agregarCandidatos(String nombre) {
+        Candidato temp = new Candidato(nombre);
+        ((ArrayList<Candidato>) super.getDatos()).add(temp);
+        notificarObservadoresEvento(0);
+    }
+
+    public void eliminarCandidatos(String nombre, String peticion) {
+        for (Candidato candidato : ((ArrayList<Candidato>)getDatos())) {
+            if (candidato.getNombre().equals(nombre)) {
+                ((ArrayList<Candidato>) getDatos()).remove(candidato);
+                break;
+            }
+        }
+        notificarObservadoresEvento(0);
+    }
+
+
+}
